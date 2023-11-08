f7616354702ba53d92e4286a0c4e26be2b167b71
jinseo0315 <kjsys12173@gmail.com>
2023-11-09 02:17:39 +0900

    main, calender, all 페이지 수정#2

--- src/pages/all.jsx
+++ src/pages/all.jsx
@@ -1,65 +1,121 @@
 // import React, {useEffect, useState} from 'react';
 import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 import "../stylesheet/all.css";
 import Header from "../components/header";
+import Calendar from 'react-calendar';
+import axios from "axios";
+
+/* axios({
+  method: 'get',
+  url: "http://3.39.75.2:8080//read/diaries",
+  data: {
+    "id": 1,
+    "date": "2023-11-08",
+    "iconUrl": "111",
+    "contents": "첫 번째",
+    "resultUrl": "111"
+  },
+}); */
 
 function All() {
 
   let navigate = useNavigate();
+  const [date, setDate] = useState(new Date());
 
-/*   const [inputValue, setInputValue] = useState('');
-  const [data, setData] = useState([]);
-
-  console.log('Data state:', data);
-
-  const handleNewsletterClick = (index, item) => {
-    navigate('/결과도출', {state: {dataIndex: index, dataItem: item}});
-  }; */
+  const [selectedicon, setselectedicon] = useState(null);
+  const [diarycontents, setdiarycontents] = useState(null);
   
+  //전체 일기 조회
+  axios.get("http://3.39.75.2:8080//read/diaries-day", {
+  })
+  //응답 성공
+  .then((response) =>{
+    setselectedicon.log(response.data.iconUrl);
+    setDate.log(response.data.date);
+    setdiarycontents.log(response.data.contents);
+
+  })
+  //응답 실패
+  .catch((error) => {
+    console.log(error);
+  })
+
   return (
     <div className="container">
       <Header/>
       
-
       <div className='all-page'>
        {/* 고민상담 모아보기 */}
         <div className="all-content">
           <div className="whitebox">
-            <div className="Emogi">emogi</div>
-            <div className="dateinfo">
-            Wednesday, November 6, 2023
+            <div className="Emogi">
+            <img src={selectedicon && selectedicon.id === 1 ? selectedicon.iconUrl : ""} alt="Emogi" />
             </div>
-            <div className="text">도란도란</div>
+            <div className="dateinfo">
+              <p>
+            <span className='bold'></span>{' '}
+            {/* 날짜 들어오면 없애기 */}
+            {date.toDateString()}
+            {date && date.id === 1 ? date.date : ""}
+              </p>
+            </div>
+            <div className="text">
+            {diarycontents && diarycontents.id === 1 ? diarycontents.contents : ""}</div>
           </div>
         </div>
 
         <div className="all-content">
           <div className="whitebox">
-            <div className="Emogi"></div>
-            <div className="dateinfo">
-            Wednesday, November 6, 2023
+            <div className="Emogi">
+            <img src={selectedicon && selectedicon.id === 2 ? selectedicon.iconUrl : ""} alt="Emogi" />
             </div>
-            <div className="text">도란도란</div>
+            <div className="dateinfo">
+              <p>
+            <span className='bold'></span>{' '}
+              {/* 날짜 들어오면 없애기 */}
+              {date.toDateString()}
+            {date && date.id === 2 ? date.date : ""}
+              </p>
+            </div>
+            <div className="text">
+            {diarycontents && diarycontents.id === 2 ? diarycontents.contents : ""}</div>
           </div>
         </div>
 
         <div className="all-content">
           <div className="whitebox">
-            <div className="Emogi"></div>
-            <div className="dateinfo">
-            Wednesday, November 6, 2023
+            <div className="Emogi">
+            <img src={selectedicon && selectedicon.id === 3 ? selectedicon.iconUrl : ""} alt="Emogi" />
             </div>
-            <div className="text">도란도란</div>
+            <div className="dateinfo">
+              <p>
+            <span className='bold'></span>{' '}
+            {/* 날짜 들어오면 없애기 */}
+            {date.toDateString()}
+            {date && date.id === 3 ? date.date : ""}
+              </p>
+            </div>
+            <div className="text">
+            {diarycontents && diarycontents.id === 3 ? diarycontents.contents : ""}</div>
           </div>
         </div>
 
         <div className="all-content">
           <div className="whitebox">
-            <div className="Emogi"></div>
-            <div className="dateinfo">
-            Wednesday, November 6, 2023
+            <div className="Emogi">
+            <img src={selectedicon && selectedicon.id === 4 ? selectedicon.iconUrl : ""} alt="Emogi" />
             </div>
-            <div className="text">도란도란</div>
+            <div className="dateinfo">
+              <p>
+            <span className='bold'></span>{' '}
+             {/* 날짜 들어오면 없애기 */}
+             {date.toDateString()}
+            {date && date.id === 4 ? date.date : ""}
+              </p>
+            </div>
+            <div className="text">
+            {diarycontents && diarycontents.id === 4 ? diarycontents.contents : ""}</div>
           </div>
         </div>
       </div>
--- src/pages/calender.jsx
+++ src/pages/calender.jsx
@@ -3,16 +3,49 @@ import { useState } from 'react';
 import Calendar from 'react-calendar';
 import "../stylesheet/calender.css";
 import 'react-calendar/dist/Calendar.css';
+import axios from "axios";
 
 import dayjs from 'dayjs';
 import Header from "../components/header";
 
+
+/* axios({
+  method: 'get',
+  url: "http://3.39.75.2:8080//read/diaries-day",
+  data: {
+    id: 1,
+    date: "2023-11-08",
+    iconUrl: "111",
+    contents: "첫 번째",
+    resultUrl: "111"
+  }
+}); */
 
 
 function Calender() {
   let navigate = useNavigate();
 
   const [date, setDate] = useState(new Date());
+
+  const [selectedicon, setselectedicon] = useState(null);
+  const [diarycontents, setdiarycontents] = useState(null);
+  
+  
+  //선택한 날의 일기 조회
+  axios.get("http://3.39.75.2:8080//read/diaries-day", {
+    params: {
+      ID: date.toDateString()
+    }
+  })
+  //응답 성공
+  .then((response) =>{
+    setselectedicon(response.data.iconUrl);
+    setdiarycontents(response.data.contents);
+  })
+  //응답 실패
+  .catch((error) => {
+    console.log(error);
+  })
 
   return (
     <div className="container">
@@ -27,7 +60,6 @@ function Calender() {
           }}/>
         </div>
       </div>
-     
       
         <div className="calender-page">
         {/* 달력 */}
@@ -49,13 +81,21 @@ function Calender() {
           >
             오늘 하루 고민을 기록해보아요!
           </button>
+
+          {/* 날짜 선택하면 일기조회 */}
           <div className="all-content">
           <div className="whitebox">
-            <div className="Emogi">emogi</div>
-            <div className="dateinfo">
-            Wednesday, November 6, 2023
+            <div className="Emogi">
+            <img src={selectedicon ? selectedicon.iconUrl : ""} alt="Emogi" />
             </div>
-            <div className="text">도란도란</div>
+            <div className="dateinfo">
+              <p>
+            <span className='bold'></span>{' '}
+            {date.toDateString()}
+              </p>
+            </div>
+            <div className="text">
+              {diarycontents ? diarycontents.contents : ""}</div>
           </div>
         </div>
       </div>
